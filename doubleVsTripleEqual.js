d4d14ecfe1bba1d42a4c6e43259aef6d642a2fc1
hamiduldiu <hamiduldiu@gmail.com>
2021-02-01 21:10:14 +0100

    doubleTriple

---
+++ doubleVsTripleEqual.js
@@ -0,0 +1,9 @@
+// == ckeck value 
+// === check value and type 
+const first=1;
+const second=true;
+if(first==second){
+    console.log("condition is true");
+}else{
+    console.log("Condition is flase");
+}
--- nullUndefined.js
+++ nullUndefined.js
@@ -11,3 +11,4 @@
 //     console.log(num2);
 // }
 
+
