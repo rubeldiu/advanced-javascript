dbfbb3a6f00ca0b46e7e3cc7f77cb46941e1a533
hamiduldiu <hamiduldiu@gmail.com>
2021-02-01 20:05:53 +0100

    null vs undefined finished

---
+++ nullUndefined.js
@@ -0,0 +1,13 @@
+// let name;
+// console.log(name);
+
+// function add(num1,num2){
+//     console.log(num1+num2) ;
+// }
+// const result=add(12,13);
+// console.log(result);
+
+// function add(num1,num2){
+//     console.log(num2);
+// }
+
---
+++ trueFalse.js
@@ -0,0 +1,19 @@
+//Falsy
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Truthy
+// '0'
+// ' '
+// []
+// {}
+
+let name;
+if(name){
+    console.log("condition is true");
+}else{
+    console.log("condition is flase");
+}
